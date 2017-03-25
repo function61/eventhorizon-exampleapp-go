8e9c52905f79f826cee8080fd6bc09b5f7d93c25
joonas.fi <pompo500@xs.fi>
2017-03-25 20:48:03 +0200

    Use WriterProxyClient to demonstrate how events can be raised and commands be implemented

--- events/usernamechanged.go
+++ events/usernamechanged.go
@@ -6,7 +6,7 @@ import (
 	"github.com/function61/pyramid-exampleapp-go/transaction"
 )
 
-// UserNameChanged {"user_id": "66cad10b", "ts": "2016-06-06 06:06:06", "new_name": "Phyllis Vance", "reason": ".."}
+// UserNameChanged {"user_id": "66cad10b", "new_name": "Phyllis Vance", "reason": "..", "ts": "2016-06-06 06:06:06"}
 type UserNameChanged struct {
 	UserId  string `json:"user_id"`
 	Ts      string `json:"ts"`
@@ -25,4 +25,9 @@ func applyUserNameChanged(tx *transaction.Tx, payload string) error {
 		ID:   e.UserId,
 		Name: e.NewName,
 	})
+}
+
+func (u *UserNameChanged) Serialize() string {
+	data, _ := json.Marshal(u)
+	return "UserNameChanged " + string(data)
 }
--- jsonrest_api.go
+++ jsonrest_api.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/function61/pyramid-exampleapp-go/events"
 	"github.com/function61/pyramid-exampleapp-go/schema"
+	"github.com/function61/pyramid/pusher/pushlib/writerproxyclient"
+	wtypes "github.com/function61/pyramid/writer/types"
+	"log"
 	"net/http"
+	"time"
 )
 
 func (a *App) setupJsonRestApi() {
+	wpc := writerproxyclient.New()
+
 	http.Handle("/users", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var users []schema.User
 		err := a.db.All(&users)
@@ -33,5 +41,45 @@ func (a *App) setupJsonRestApi() {
 		if err := encoder.Encode(companies); err != nil {
 			panic(err)
 		}
+	}))
+
+	// example of raising an event from inside our application and sending it to
+	// Pyramid. it will eventually reach our Pusher endpoint and get committed to
+	// our database.
+	//
+	// NOTE: normally this command would take in JSON but I was feeling lazy and
+	//       I just used URL params...
+	http.Handle("/command/change_user_name", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userId := r.URL.Query().Get("user")
+		var user schema.User
+
+		// make sure the user exists
+		if err := a.db.One("ID", userId, &user); err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+
+		log.Printf("App: changing username for previous name = %s", user.Name)
+
+		// create an event
+		userNameChanged := &events.UserNameChanged{
+			UserId:  user.ID,
+			NewName: r.URL.Query().Get("new_name"),
+			Reason:  "just an example",
+			Ts:      time.Now().Format("2006-01-02 15:04:05"),
+		}
+
+		// append it to Pyramid
+		output, err := wpc.Append(&wtypes.AppendToStreamRequest{
+			Stream: "/example",
+			Lines:  []string{userNameChanged.Serialize()},
+		})
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Write([]byte(fmt.Sprintf("OK; offset = %s", output.Offset)))
 	}))
 }
